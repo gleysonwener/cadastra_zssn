8d6561f899e0a6843575aa1a5e046b13bd3597f9
gleysonwener <gleysonwener3@gmail.com>
2022-08-12 22:44:59 -0300

    deploy

--- zssn/wsgi.py
+++ zssn/wsgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from dj_database_url import parse as dburl
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zssn.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+
+# application = get_wsgi_application()
